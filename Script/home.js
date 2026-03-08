bd25fb7f6e4a18276f8bc7069a91f93436fa27e5
sabbirRashed <srsabbirrahman12>
2026-03-09 00:24:48 +0600

    fetched and displayed search issue

--- Script/home.js
+++ Script/home.js
@@ -29,38 +29,33 @@ const createElement = (arr) => {
     return (htmlElement.join(" "));
 }
 
-const activeMaker = (id = "all-tab") => {
+const activeRemover = () => {
     const tabBtn = document.querySelectorAll(".active");
     tabBtn.forEach(item => {
         item.classList.remove("active");
     });
-    document.getElementById(id).classList.add("active");
-}
 
-const manageContainer = (id) => {
-    const containers = document.querySelectorAll(".issue-container");
-    containers.forEach(item => item.classList.add("hidden"));
-
-    document.getElementById(id).classList.remove("hidden");
 }
 
 const manageSpinner = (status) => {
     if (status === true) {
         document.getElementById("spinner").classList.remove("hidden");
-        document.getElementById("containers-parent").classList.add("hidden");
+        document.getElementById("issue-container").classList.add("hidden");
     } else {
-        document.getElementById("containers-parent").classList.remove("hidden");
+        document.getElementById("issue-container").classList.remove("hidden");
         document.getElementById("spinner").classList.add("hidden");
     }
 }
 
-const loadAllIssues = (id) => {
+const loadAllIssues = (id = "all-tab") => {
     manageSpinner(true);
     const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
     fetch(url)
         .then((res) => res.json()
             .then((data) => {
-                activeMaker(id);
+
+                activeRemover();
+                document.getElementById(id).classList.add("active");
                 displayIssues(data.data);
             })
 
@@ -75,20 +70,67 @@ const loadIssueInfo = (id) => {
         )
 }
 
-// data": {
-// "id": 33,
-// "title": "Add bulk operations support",
-// "description": "Allow users to perform bulk actions like delete, update status on multiple items at once.",
-// "status": "open",
-// "labels": [
-// "enhancement"
-// ],
-// "priority": "low",
-// "author": "bulk_barry",
-// "assignee": "",
-// "createdAt": "2024-02-02T10:00:00Z",
-// "updatedAt": "2024-02-02T10:00:00Z"
-// }
+const loadSearchIssue = (search) => {
+    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${search}`;
+
+    fetch(url)
+        .then(res => res.json()
+            .then(data => {
+                console.log(data.data)
+                displaySearchIssues(data.data)
+            })
+
+        )
+
+}
+
+
+const displaySearchIssues = (data) => {
+
+    const issueCount = document.getElementById("issue-count");
+    const issueContainer = document.getElementById("issue-container");
+    issueContainer.innerHTML = "";
+
+    data.forEach(item => {
+
+        const div = document.createElement("div");
+        div.innerHTML = `
+                <div onclick="loadIssueInfo(${item.id})" class="card bg-white rounded-lg shadow-md h-full ${item.status.toLowerCase() === "open" ? "border border-green-200 border-t-4 border-t-green-500" : "border border-purple-200 border-t-4 border-t-purple-500"}">
+                    <div class="p-5">
+                        <div class="flex justify-between items-center">
+                            ${imageMaker(item.status)}
+                            <p class="text-sm font-semibold ${item.priority.toLowerCase() === "high" ? "text-red-500 bg-red-100" : item.priority.toLowerCase() === "medium" ? "text-amber-500 bg-amber-100" : item.priority.toLowerCase() === "low" ? "text-gray-500 bg-gray-100" : ""} px-6 py-1 rounded-2xl">${item.priority.toUpperCase()}</p>
+                        </div>
+                        <h2 class="font-semibold text-[#1F2937] mt-3">${item.title}</h2>
+                        <p class="text-sm text-[#64748B] mt-2">${item.description}</p>
+                        <div class="mt-3.5 flex flex-wrap gap-1">
+                            ${createElement(item.labels)}
+                        </div>
+                    </div>
+                    <div class=" border-t border-gray-200">
+                        <div class=" p-5 text-sm text-[#64748B] space-y-1">
+                            <div class="flex justify-between items-center">
+                                <h3>#${item.id} by ${item.author}</h3>
+                                <p>${new Date(item.createdAt).toLocaleDateString()}</p>
+                            </div>
+                            <div class="flex justify-between items-center">
+                                <h3>Assigne: ${item.assignee ? item.assignee : "Unassigned"}</h3> 
+                                <p class="text-right">Update: ${new Date(item.updatedAt).toLocaleDateString()}</p>
+                            </div>                         
+                        </div>
+                    </div>
+                </div>
+
+                `
+
+        issueContainer.appendChild(div);
+
+    })
+    issueCount.innerText = issueContainer.children.length;
+    manageSpinner(false);
+
+}
+
 
 const displayIssueInfo = (info) => {
 
@@ -97,8 +139,8 @@ const displayIssueInfo = (info) => {
                 <div class="space-y-2">
                     <h2 class="text-2xl font-bold text-[#1F2937]">${info.title}</h2>
                     <ul class="flex items-center gap-7 text-sm text-[#64748B]">
-                        <li class="px-5 py-1 text-white ${info.status === "open"? "bg-green-500": "bg-purple-500"} rounded-2xl text-white">${info.status === "open"? "opened": "closed"}</li>
-                        <li class="list-disc">${info.status === "open"? "opened": "closed"} by ${info.assignee}</li>
+                        <li class="px-5 py-1 text-white ${info.status === "open" ? "bg-green-500" : "bg-purple-500"} rounded-2xl text-white">${info.status === "open" ? "opened" : "closed"}</li>
+                        <li class="list-disc">${info.status === "open" ? "opened" : "closed"} by ${info.assignee ? info.assignee : "Unknown"}</li>
                         <li class="list-disc">${new Date(info.updatedAt).toLocaleDateString()}</li>
                     </ul>
                 </div>
@@ -109,18 +151,17 @@ const displayIssueInfo = (info) => {
 
                 <p class="text-[#64748B]">${info.description}</p>
 
-                <div class="grid grid-cols-2 p-8 bg-base-200 rounded-lg">
+                <div class="grid grid-cols-2 p-5 bg-base-200 rounded-lg">
                     <div>
                         <p class="text-[#64748B]">Assigne:</p>
-                        <p class="text-[#1F2937] font-semibold">${info.assignee? info.assignee: "Unassigned"}</p>
+                        <p class="text-[#1F2937] font-semibold">${info.assignee ? info.assignee : "Unassigned"}</p>
                     </div>
                     <div>
                         <p class="text-[#64748B]">Priority:</p>
-                        <span class="text-sm text-white font-semibol ${
-                            info.priority === "high"? "bg-red-400"
-                            :info.priority === "medium"? "bg-amber-400"
-                            :info.priority === "low"? "bg-gray-400"
-                            : ""} px-6 py-1 rounded-2xl">${info.priority.toUpperCase()}</span>
+                        <span class="text-sm text-white font-semibol ${info.priority === "high" ? "bg-red-400"
+            : info.priority === "medium" ? "bg-amber-400"
+                : info.priority === "low" ? "bg-gray-400"
+                    : ""} px-6 py-1 rounded-2xl">${info.priority.toUpperCase()}</span>
                     </div>
                 </div>
     
@@ -129,16 +170,15 @@ const displayIssueInfo = (info) => {
 
 }
 
+
 const displayIssues = (data) => {
 
+    const searcField = document.getElementById("search-input");
     const issueCount = document.getElementById("issue-count");
-    const allIssueContainer = document.getElementById("all-container");
-    const openIssueContainer = document.getElementById("open-container");
-    const closedIssueContainer = document.getElementById("closed-container");
+    const issueContainer = document.getElementById("issue-container");
 
-    allIssueContainer.innerHTML = "";
-    openIssueContainer.innerHTML = "";
-    closedIssueContainer.innerHTML = "";
+    searcField.value = "";
+    issueContainer.innerHTML = "";
 
     data.forEach(item => {
 
@@ -175,31 +215,30 @@ const displayIssues = (data) => {
         const activeTab = document.querySelector(".active").id;
 
         if (activeTab === "all-tab") {
-            manageContainer("all-container");
-
-            allIssueContainer.appendChild(div);
-            issueCount.innerText = allIssueContainer.children.length;
+            issueContainer.appendChild(div);
         }
-
-        if (activeTab === "open-tab") {
-            manageContainer("open-container");
-
-            if (item.status.toLowerCase() === "open") {
-                openIssueContainer.appendChild(div);
-                issueCount.innerText = openIssueContainer.children.length;
-            }
+        else if (activeTab === "open-tab" && item.status.toLowerCase() === "open") {
+            issueContainer.appendChild(div);
         }
-        if (activeTab === "closed-tab") {
-            manageContainer("closed-container");
-
-            if (item.status.toLowerCase() === "closed") {
-                closedIssueContainer.appendChild(div);
-                issueCount.innerText = closedIssueContainer.children.length;
-            }
+        else if (activeTab === "closed-tab" && item.status.toLowerCase() === "closed") {
+            issueContainer.appendChild(div);
         }
     })
+    issueCount.innerText = issueContainer.children.length;
     manageSpinner(false);
 
 }
+
+document.getElementById("search-btn")
+    .addEventListener("click", (event) => {
+        event.preventDefault();
+
+        const searchValue = document.getElementById("search-input").value;
+        if(searchValue === ""){
+            return;
+        }
+        activeRemover();
+        loadSearchIssue(searchValue);
+    })
 
 loadAllIssues()
